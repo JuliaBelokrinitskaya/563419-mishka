23b71505e553a63fb14b7c9fa185bf1efb480b30
Julia Belokrinitskaya <bel_julia@mail.ru>
2018-02-10 06:44:58 +0300

    Fix JS and form errors

--- source/js/app.js
+++ source/js/app.js
@@ -5,30 +5,36 @@ var btnFeatured = document.querySelector(".featured-product__btn");
 var overlay = document.querySelector(".overlay");
 var modalToCart = document.querySelector(".modal--to-cart");
 
-navMain.classList.add("main-nav--opened");
+if (navMain) {
+  navMain.classList.add("main-nav--closed");
+};
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
-});
+if (navToggle && navMain) {
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
+  });
+};
 
-btnFeatured.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  overlay.classList.add("overlay--show");
-  modalToCart.classList.add("modal--show");
-});
+if (btnFeatured && overlay && modalToCart) {
+  btnFeatured.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    overlay.classList.add("overlay--show");
+    modalToCart.classList.add("modal--show");
+  });
+};
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    if (modalToCart.classList.contains("modal--show")) {
+    if (modalToCart && modalToCart.classList.contains("modal--show")) {
       modalToCart.classList.remove("modal--show");
     }
-    if (overlay.classList.contains("overlay--show")) {
+    if (overlay && overlay.classList.contains("overlay--show")) {
       overlay.classList.remove("overlay--show");
     }
   }
