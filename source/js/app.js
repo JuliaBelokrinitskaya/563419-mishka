38aa9ff495fbf7906e7c5d06b3763a07d9c9bbd8
Julia Belokrinitskaya <bel_julia@mail.ru>
2018-02-11 17:09:30 +0300

    Add behavior to order btns

--- source/js/app.js
+++ source/js/app.js
@@ -5,29 +5,38 @@ var btnFeatured = document.querySelector(".featured-product__btn");
 var overlay = document.querySelector(".overlay");
 var modalToCart = document.querySelector(".modal--to-cart");
 
-if (navMain) {
-  navMain.classList.add("main-nav--closed");
-};
+var btnsToCart = document.querySelectorAll(".product__to-cart");
 
-if (navToggle && navMain) {
-  navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
-    } else {
-      navMain.classList.add('main-nav--closed');
-      navMain.classList.remove('main-nav--opened');
-    }
-  });
-};
 
-if (btnFeatured && overlay && modalToCart) {
+navMain.classList.add("main-nav--closed");
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+if (btnFeatured) {
   btnFeatured.addEventListener("click", function (evt) {
     evt.preventDefault();
     overlay.classList.add("overlay--show");
     modalToCart.classList.add("modal--show");
   });
 };
+
+if (btnsToCart) {
+  for (var i = 0; i < btnsToCart.length; i++) {
+    btnsToCart[i].addEventListener("click", function (evt) {
+      evt.preventDefault();
+      overlay.classList.add("overlay--show");
+      modalToCart.classList.add("modal--show");
+    });
+  }
+}
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
