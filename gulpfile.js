a0b03aa82ad1e89ead8b98382cf1ab5c43c48598
Julia Belokrinitskaya <bel_julia@mail.ru>
2018-02-16 12:18:59 +0300

    Finish automatization and optimization

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var uglify = require("gulp-uglify");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
 var rename = require("gulp-rename");
@@ -13,6 +15,7 @@ var svgstore = require("gulp-svgstore");
 var svgmin = require("gulp-svgmin");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
+var htmlmin = require("gulp-htmlmin");
 var server = require("browser-sync").create();
 var run = require("run-sequence");
 
@@ -39,14 +42,31 @@ gulp.task("style", function() {
       autoprefixer()
     ]))
     .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("scripts", function() {
+  return gulp.src("build/js/*.js")
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest("build/js"))
     .pipe(server.stream());
 });
 
 gulp.task("images", function() {
   return gulp.src("build/img/**/*.{png,jpg,svg}")
     .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({
+        optimizationLevel: 3
+      }),
+      imagemin.jpegtran({
+        progressive: true
+      }),
       imagemin.svgo()
     ]))
     .pipe(gulp.dest("build/img"));
@@ -54,7 +74,9 @@ gulp.task("images", function() {
 
 gulp.task("webp", function() {
   return gulp.src("build/img/**/*.{png,jpg}")
-    .pipe(webp({quality: 90}))
+    .pipe(webp({
+      quality: 90
+    }))
     .pipe(gulp.dest("build/img/webp"));
 });
 
@@ -79,7 +101,11 @@ gulp.task("html", function() {
     .pipe(posthtml([
       include()
     ]))
-    .pipe(gulp.dest("build"));
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest("build"))
+    .pipe(server.stream());
 });
 
 gulp.task("build", function(done) {
@@ -87,6 +113,7 @@ gulp.task("build", function(done) {
     "clean",
     "copy",
     "style",
+    "scripts",
     "images",
     "webp",
     "sprite",
@@ -97,7 +124,11 @@ gulp.task("build", function(done) {
 
 gulp.task("serve", function() {
   server.init({
-    server: "build/"
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
